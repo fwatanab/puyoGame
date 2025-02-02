abe287d3ef8e581c54dda962f5ceb113af5c67e4
fwatanab <fwatanab@student.42tokyo.jp>
2025-02-02 15:38:52 +0900

    Rendererの子クラスとしてGame関連の描画を扱うためのクラス

---
+++ include/GameRenderer.hpp
@@ -0,0 +1,22 @@
+#ifndef GAMERENDERER_HPP
+#define GAMERENDERER_HPP
+
+#include "Renderer.hpp"
+#include "Board.hpp"
+#include "ImageManager.hpp"
+#include "PuyoPair.hpp"
+
+class	GameRenderer : public Renderer {
+	public:
+		GameRenderer(const std::string& title);
+
+		void	renderBoard(const Board& board, const ImageManager& imageManager);
+		void	renderPuyoPair(const PuyoPair& puyoPair, const ImageManager& imageManager);
+
+		SDL_Rect	getDestRect(int x, int y) const;
+
+		void	render() override;
+
+};
+
+#endif

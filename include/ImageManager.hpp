ea27d1d16999cd3d154b820cb99bab1c356847ac
fwatanab <fwatanab@student.42tokyo.jp>
2025-02-04 16:08:46 +0900

    テクスチャの保管のアプローチを変更

--- include/ImageManager.hpp
+++ include/ImageManager.hpp
@@ -7,29 +7,55 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "Puyo.hpp"
+#include "Menu.hpp"
 #include "GeneratedConfig.hpp"
+
+// ゲーム用のテクスチャを表す列挙型
+enum class	GameTexture {
+	BACKGROUND,
+	RED_PUYO,
+	BLUE_PUYO,
+	GREEN_PUYO,
+	YELLOW_PUYO,
+	PURPLE_PUYO,
+	GARBAGE_PUYO,
+	WALL
+};
+
+// メニュー用のテクスチャを表す列挙型
+enum class	MenuTexture {
+	BACKGROUND,
+	FRAME,
+	SOLO_MODE,
+	DUO_MODE,
+	EXIT
+};
 
 class	ImageManager {
 	public:
 		ImageManager();
 		~ImageManager();
 
-		// 初期化
+		// すべてのテクスチャを初期化
 		void	initTextures(SDL_Renderer* renderer);
+		
+		// ゲーム関連のテクスチャ管理
+		void	loadGameTexture(SDL_Renderer* renderer, GameTexture type, const std::string& filePath);
+		SDL_Texture*	getGameTexture(GameTexture type) const;
+		void	clearGameTextures();
+		
+		// メニュー関連のテクスチャ管理
+		void	loadMenuTexture(SDL_Renderer* renderer, MenuTexture type, const std::string& filePath);
+		SDL_Texture*	getMenuTexture(MenuTexture type) const;
+		void	clearMenuTextures();
 
-		// テクスチャの読み込み
-		void	loadTexture(SDL_Renderer* renderer, PuyoColor color, const std::string& filePath);
-
-		// テクスチャを取得する
-		SDL_Texture*	getTexture(PuyoColor key) const;
-
-		// テクスチャの削除
-		void	unloadTexture(PuyoColor key);
-
-		void	clear();
+		// 変換メソッド
+		SDL_Texture*	getTextureForPuyo(PuyoColor color) const;
+		SDL_Texture*	getTextureForMenu(MenuOption option) const;
 
 	private:
-		std::map<PuyoColor, SDL_Texture*>	textures_; // テクスチャのマップ
+		std::map<GameTexture, SDL_Texture*>	gameTextures_;
+		std::map<MenuTexture, SDL_Texture*>	menuTextures_;
 };
 
 #endif
--- src/graphics/ImageManager.cpp
+++ src/graphics/ImageManager.cpp
@@ -3,61 +3,127 @@
 ImageManager::ImageManager() {}
 
 ImageManager::~ImageManager() {
-	clear();
+	clearGameTextures();
+	clearMenuTextures();
 }
 
 void	ImageManager::initTextures(SDL_Renderer* renderer) {
-	// Config ファイルから画像をロード
-	loadTexture(renderer, PuyoColor::RED, PUYO_RED_IMAGE);
-	loadTexture(renderer, PuyoColor::BLUE, PUYO_BLUE_IMAGE);
-	loadTexture(renderer, PuyoColor::GREEN, PUYO_GREEN_IMAGE);
-	loadTexture(renderer, PuyoColor::YELLOW, PUYO_YELLOW_IMAGE);
-	loadTexture(renderer, PuyoColor::PURPLE, PUYO_PURPLE_IMAGE);
-	loadTexture(renderer, PuyoColor::WALL, PUYO_WALL_IMAGE);
+	// ぷよ関連のテクスチャをロード
+	loadGameTexture(renderer, GameTexture::BACKGROUND, GAME_BACKGROUND_IMAGE);
+	loadGameTexture(renderer, GameTexture::RED_PUYO, PUYO_RED_IMAGE);
+	loadGameTexture(renderer, GameTexture::BLUE_PUYO, PUYO_BLUE_IMAGE);
+	loadGameTexture(renderer, GameTexture::GREEN_PUYO, PUYO_GREEN_IMAGE);
+	loadGameTexture(renderer, GameTexture::YELLOW_PUYO, PUYO_YELLOW_IMAGE);
+	loadGameTexture(renderer, GameTexture::PURPLE_PUYO, PUYO_PURPLE_IMAGE);
+//	loadGameTexture(renderer, GameTexture::GARBAGE_PUYO, PUYO_GARBAGE_IMAGE);
+	loadGameTexture(renderer, GameTexture::WALL, GAME_WALL_IMAGE);
+
+	// メニュー関連テクスチャをロード
+//	loadMenuTexture(renderer, MenuTexture::BACKGROUND, MENU_BACKGROUND);
+//	loadMenuTexture(renderer, MenuTexture::FRAME, MENU_FRAME_IMAGE);
+//	loadMenuTexture(renderer, MenuTexture::SOLO_MODE, MENU_ITEM_SOLO_MODE);
+//	loadMenuTexture(renderer, MenuTexture::DUO_MODE, MENU_ITEM_DUO_MODE);
+//	loadMenuTexture(renderer, MenuTexture::EXIT, MENU_ITEM_EXIT);
 }
 
 
-void	ImageManager::loadTexture(SDL_Renderer* renderer, PuyoColor color, const std::string& filePath) {
-	if (textures_.find(color) != textures_.end()) {
-		throw std::runtime_error("Texture for this color already exists.");
+void	ImageManager::loadGameTexture(SDL_Renderer* renderer, GameTexture type, const std::string& filePath) {
+	if (gameTextures_.find(type) != gameTextures_.end()) {
+		throw std::runtime_error("Game texture already exists: " + filePath);
 	}
 
-	SDL_Surface* surface = IMG_Load(filePath.c_str());
+	SDL_Surface*	surface = IMG_Load(filePath.c_str());
 	if (!surface) {
 		throw std::runtime_error("Failed to load image: " + filePath);
 	}
 
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_Texture*	texture = SDL_CreateTextureFromSurface(renderer, surface);
 	SDL_FreeSurface(surface);
 
 	if (!texture) {
 		throw std::runtime_error("Failed to create texture for: " + filePath);
 	}
 
-	textures_[color] = texture;
+	gameTextures_[type] = texture;
 }
 
-SDL_Texture* ImageManager::getTexture(PuyoColor color) const {
-    auto it = textures_.find(color);
-    if (it == textures_.end()) {
-//        throw std::runtime_error("Texture for color not found.");
+SDL_Texture*	ImageManager::getGameTexture(GameTexture type) const {
+	std::map<GameTexture, SDL_Texture*>::const_iterator	it = gameTextures_.find(type);
+	if (it == gameTextures_.end()) {
 		return nullptr;
-    }
-    return it->second;
+	}
+	return it->second;
 }
 
-void	ImageManager::unloadTexture(PuyoColor key) {
-	std::map<PuyoColor, SDL_Texture*>::iterator	it;
-	if (it = textures_.find(key); it != textures_.end()) {
+void	ImageManager::clearGameTextures() {
+	std::map<GameTexture, SDL_Texture*>::iterator	it;
+	for (it = gameTextures_.begin(); it != gameTextures_.end(); ++it) {
 		SDL_DestroyTexture(it->second);
-		textures_.erase(it);
+	}
+	gameTextures_.clear();
+}
+
+void	ImageManager::loadMenuTexture(SDL_Renderer* renderer, MenuTexture type, const std::string& filePath) {
+	if (menuTextures_.find(type) != menuTextures_.end()) {
+		throw std::runtime_error("Menu texture already exists: " + filePath);
+	}
+	SDL_Surface*	surface = IMG_Load(filePath.c_str());
+	if (!surface) {
+		throw std::runtime_error("Failed to load menu image: " + filePath);
+	}
+
+	SDL_Texture*	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	if (!texture) {
+		throw std::runtime_error("Failed to create menu texture: " + filePath);
+	}
+
+	menuTextures_[type] = texture;
+}
+
+SDL_Texture*	ImageManager::getMenuTexture(MenuTexture type) const {
+	std::map<MenuTexture, SDL_Texture*>::const_iterator	it = menuTextures_.find(type);
+	if (it == menuTextures_.end()) {
+		return nullptr;
+	}
+	return it->second;
+}
+
+void	ImageManager::clearMenuTextures() {
+	std::map<MenuTexture, SDL_Texture*>::iterator	it;
+	for (it = menuTextures_.begin(); it != menuTextures_.end(); ++it) {
+		SDL_DestroyTexture(it->second);
+	}
+	menuTextures_.clear();
+}
+
+SDL_Texture*	ImageManager::getTextureForPuyo(PuyoColor color) const {
+	switch (color) {
+		case PuyoColor::RED:
+			return getGameTexture(GameTexture::RED_PUYO);
+		case PuyoColor::BLUE:
+			return getGameTexture(GameTexture::BLUE_PUYO);
+		case PuyoColor::GREEN:
+			return getGameTexture(GameTexture::GREEN_PUYO);
+		case PuyoColor::YELLOW:
+			return getGameTexture(GameTexture::YELLOW_PUYO);
+		case PuyoColor::PURPLE:
+			return getGameTexture(GameTexture::PURPLE_PUYO);
+//		case PuyoColor::GARBAGE:
+//			return getGameTexture(GameTexture::GARBAGE_PUYO);
+		case PuyoColor::EMPTY:
+			return nullptr;
+		default:
+			throw std::runtime_error("Invalid PuyoColor.");
 	}
 }
 
-void	ImageManager::clear() {
-	std::map<PuyoColor, SDL_Texture*>::iterator	it;
-	for (it = textures_.begin(); it != textures_.end(); ++it) {
-		SDL_DestroyTexture(it->second);
+SDL_Texture*	ImageManager::getTextureForMenu(MenuOption option) const {
+	switch (option) {
+		case MenuOption::SOLO:
+			return getMenuTexture(MenuTexture::SOLO_MODE);
+		default:
+			throw std::runtime_error("Invalid MenuOption.");
 	}
-	textures_.clear();
 }
